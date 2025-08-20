c8b39c79169dc14722d0ec6ca5bdb70f15be9fee
Max Chivers <maxchivers@macbookpro.home>
2025-08-20 15:13:33 +0100

    Add telescope shortcut for dotfiles

--- init.lua
+++ init.lua
@@ -451,6 +451,11 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
+      -- Shortcut for searching your dotfiles
+      vim.keymap.set('n', '<leader>sD', function()
+        builtin.find_files { cwd = '~/.config/'}
+      end, { desc = '[S]earch [D]otfiles' })
     end,
   },
 
@@ -846,13 +851,13 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'savq/melange-nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'melange'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
